7a1a0691d85dd07be8d4babc3872a9aed7b3ed6f
Eloise B. <eloisebellorio1999@gmail.com>
2026-03-01 11:06:31 -0300

    Adding images and sounds

--- code/game.py
+++ code/game.py
@@ -1,12 +1,13 @@
 import pygame
 
 from code.Menu import Menu
+from code.const import WIN_HEIGHT, WIN_WIDTH
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode((600, 480))
+        self.window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT ))
 
     def run(self):
         while True:
