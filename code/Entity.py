d9d153e624729f1ba26a90d3ec395b2af12c0c03
eloiseb <eloisebellorio1999@gmail.com>
2026-03-14 14:54:36 -0300

    Creating Mediator Pattern, Enemies Destruction

--- code/Entity.py
+++ code/Entity.py
@@ -1,6 +1,8 @@
 import pygame.image
 from abc import ABC, abstractmethod
 
+from code.const import ENTITY_HEALTH
+
 
 class Entity(ABC):
 
@@ -9,7 +11,7 @@ class Entity(ABC):
         self.surf = pygame.image.load("./asset/" + name + ".png").convert_alpha()
         self.rect = self.surf.get_rect(left=position[0], top=position[1])
         self.speed = 0
-
+        self.health = ENTITY_HEALTH[self.name]
     @abstractmethod
     def move(self):
         pass
--- code/Level.py
+++ code/Level.py
@@ -6,6 +6,7 @@ from pygame.font import Font
 
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
+from code.EntityMediator import EntityMediator
 from code.const import COLOR_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
 
 
@@ -64,6 +65,11 @@ class Level:
             )
 
             pygame.display.flip()
+
+            EntityMediator.verify_collision(entity_list=self.entity_list)
+            EntityMediator.verify_health(entity_list=self.entity_list)
+            pass
+
 
     def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
